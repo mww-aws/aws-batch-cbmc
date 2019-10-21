89aea89fb9ec727b94f32f898c2df2203de933de
Mike Whalen <mww@amazon.com>
2019-10-20 19:49:00 -0500

    Adding support for RAM disks

--- bin/boot.py
+++ bin/boot.py
@@ -3,21 +3,37 @@
 # Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-"""Copy cbmc-batch package from S3 and launch it"""
+"""Create RAM disks, copy cbmc-batch package from S3 and launch it"""
 
 import json
 
 import options
 import package
+import ramdisk
+
 
 def boot():
-    """Copy cbmc-batch package from S3 and launch it"""
+    """Create RAM disks, copy cbmc-batch package from S3 and launch it
+        NOTE: RAM disks use only as much memory as consumed by files currently resident.
+        The 3GB limit is perhaps excessive but should not lead to problems in practice if
+        file sizes are reasonable"""
     opts = options.docker_options()
     print("Booting with options " + json.dumps(opts))
+
+    RAM_DISK_DIRS = ['cbmc-batch', 'cbmc', 'cbmc-viewer',
+                     opts['blddir'], opts['wsdir'], opts['srcdir'], opts['outdir']]
+    RAM_DISK_SIZE = 3072
+    print("Creating RAM disks: {} with size {} ".format(str(RAM_DISK_DIRS), RAM_DISK_SIZE))
+    ramdisk.mount_ramdisks(RAM_DISK_DIRS, RAM_DISK_SIZE)
 
     package.copy('cbmc-batch', opts['pkgbucket'], opts['batchpkg'])
     package.install('cbmc-batch', opts['batchpkg'], 'cbmc-batch')
     package.launch('cbmc-batch', 'docker.py', ['--jsons', json.dumps(opts)])
+
+    print("Tasks using RAM disks: ")
+    ramdisk.tasks_using_ramdisks(RAM_DISK_DIRS)
+    print("Unmounting RAM disks")
+    ramdisk.unmount_ramdisks(RAM_DISK_DIRS)
 
 if __name__ == "__main__":
     boot()
---
+++ bin/ramdisk.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+"""Ramdisk support for Linux systems.
+    Note: tmpfs ram disks have a maximum size, but only use ram up to the amount required by the
+    files stored in the ram disk.  Therefore, it is o.k. to create several ram disks and even to
+    mount a ram disk underneath another ram disk in terms of memory consumption.
+
+    Note that if the tmpfs RAM disks become too large, they will adversely affect performance by using
+    swap space (like any other RAM-resident task), likely making performance very slow.
+"""
+
+import os
+import sys
+import subprocess
+
+def normalize_folders(folders):
+    """Normalize folders in a list of folders."""
+    return [os.path.abspath(folder) for folder in folders]
+
+def mount_ramdisk(folder, megabytes=1024):
+    """Mount a ramdisk on a folder.
+
+    Mount a ramdisk of a size given by megabytes on a folder.
+    """
+    os.makedirs(folder)
+    cmd = ['sudo',
+           'mount', '-t', 'tmpfs', '-o', 'size={}M'.format(megabytes), 'tmpfs',
+           folder]
+    subprocess.run(cmd, check=True)
+
+def unmount_ramdisk(folder):
+    """
+    Unmount a ramdisk
+    :param folder:
+    :return:
+    """
+    cmd = ['sudo',
+           'umount', folder]
+    subprocess.run(cmd, check=True)
+
+def mount_ramdisks(folders, megabytes=1024):
+    """Mount ramdisks on folders.
+
+    Mount a ramdisk of a size given by megabytes on each folder in
+    folders, taking care to normalize folder names, ignore duplicates,
+    and create the folders in the right order.
+    """
+    folders = [os.path.abspath(folder) for folder in folders]
+    for folder in sorted(set(folders)):
+        mount_ramdisk(folder, megabytes)
+
+def unmount_ramdisks(folders):
+    """unmount ramdisks on folders.
+
+    unmount a ramdisk of a size given by megabytes on each folder in
+    folders, taking care to normalize folder names, ignore duplicates,
+    and create the folders in the right order.
+    """
+
+    folders = [os.path.abspath(folder) for folder in folders]
+    for folder in sorted(set(folders)):
+        unmount_ramdisk(folder)
+
+def tasks_using_ramdisk(folder):
+    cmd = ['sudo',
+           'lsof', '-n', folder]
+    subprocess.run(cmd, check=True)
+
+def tasks_using_ramdisks(folders):
+    folders = [os.path.abspath(folder) for folder in folders]
+    for folder in sorted(set(folders)):
+        tasks_using_ramdisk(folder)
+
+
+if __name__ == '__main__':
+    mount_ramdisks(sys.argv[1:])
